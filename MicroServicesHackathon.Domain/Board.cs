571f4733ec3db8f94acee2b72b1383369d0b60c6
Ivan Zlatev <i.zlatev@marketinvoice.com>
2014-11-29 14:31:14 +0000

    Checked in the domain model.

---
+++ MicroServicesHackathon.Domain/Board.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MicroServicesHackathon.Domain
+{
+    public class Board
+    {
+        private List<Movement> _movements;
+
+        public Board() : this(new List<Movement>())
+        {
+        }
+
+        public Board(List<Movement> movements)
+        {
+            if (movements == null)
+                throw new ArgumentNullException("movements", "movements is null.");
+
+            Size = 3;
+            _movements = movements;
+        }
+
+        public string GameId { get; set; }
+
+        public ICollection<Movement> Movements {
+            get { return _movements; }
+        }
+
+        public int Size { get; private set; }
+
+        public bool MakeMove(Movement movement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MicroServicesHackathon.Domain/Movement.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroServicesHackathon.Domain
+{
+    public class Movement
+    {
+        public string PlayerId { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
