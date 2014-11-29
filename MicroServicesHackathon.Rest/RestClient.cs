3240df37474304f4ff87123c0d71bad66aee05c4
Ivan Zlatev <i.zlatev@marketinvoice.com>
2014-11-29 15:49:36 +0000

    Finish implementing the REST api

--- MicroServicesHackathon.Rest/RestClient.cs
+++ MicroServicesHackathon.Rest/RestClient.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using MicroServicesHackathon.Facts;
 using RestSharp;
+using System.Net;
 
 namespace MicroServicesHackathon.Rest
 {
@@ -17,25 +18,53 @@ namespace MicroServicesHackathon.Rest
 
         public string Subscribe(string topic)
         {
-            //RestRequest request = new RestRequest("/topics/{topic_name}/subscriptions", Method.POST);
-            //request.AddUrlSegment("topic_name", topic);
+            RestRequest request = new RestRequest("/topics/{topic_name}/subscriptions", Method.POST);
+            request.AddUrlSegment("topic_name", topic);
 
-            throw new NotImplementedException();
+            IRestResponse<Subscription> response = _client.Post<Subscription>(request);
+            if (IsPostSuccessful(response))
+                return response.Data.subscription_id;
+            else
+                return null;
         }
 
-        public T NextFact<T>(string topic, string subscription) where T:Fact
+        public TFact NextFact<TFact>(string topic, string subscription, int waitSeconds = 1) where TFact : Fact, new()
         {
-            throw new NotImplementedException();
+            if (waitSeconds <= 1)
+                throw new ArgumentOutOfRangeException("waitSeconds", "waitSeconds has to > 0");
+
+            RestRequest request = new RestRequest("/topics/{topic_name}/subscriptions/{subscription_id}/next", Method.GET);
+            request.AddUrlSegment("topic_name", topic);
+            request.AddUrlSegment("subscription_id", subscription);
+            request.AddHeader("Patience", waitSeconds.ToString());
+
+            IRestResponse<TFact> response = _client.Get<TFact>(request);
+            if (response.ResponseStatus == ResponseStatus.Completed)
+                return (TFact) response.Data;
+            else
+                return null;
         }
 
-        public void PostFact(string topic, Fact fact)
+
+        public bool PostFact(string topic, Fact fact)
         {
             RestRequest request = new RestRequest("/topics/{topic_name}/facts", Method.POST);
             request.AddUrlSegment("topic_name", topic);
 
             request.AddJsonBody(fact);
 
-            _client.Post(request);
+            var response = _client.Post(request);
+
+            return IsPostSuccessful(response);
         }
+
+        #region Utils
+
+        private static bool IsPostSuccessful(IRestResponse response)
+        {
+            return response.ResponseStatus == ResponseStatus.Completed && response.StatusCode == HttpStatusCode.Accepted;
+        }
+
+        #endregion
     }
 }
